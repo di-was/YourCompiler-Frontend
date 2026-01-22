2f3eabbfca04e421e1b8c19100b7f0bad83e5383
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-22 12:58:48 +1100

    navbar components integration

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import LanguageSelector from "@/components/LanguageSelector";
+import VersionSelector from "@/components/VersionSelector";
+
 export default function Navbar() {
     return (
         <nav className="w-screen h-10 bg-white flex items-center justify-left px-4 gap-4">
-            <h1>YourCompiler</h1>
-            <h2>Version</h2>
+            <LanguageSelector />
+            <VersionSelector />
         </nav>
     );
 }
