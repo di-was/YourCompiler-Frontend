0edc9fb20be3b8683c88e65dc0991b06d0fa6928
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-22 12:51:30 +1100

    navbar skeleton

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Navbar() {
+    return (
+        <nav className="w-screen h-10 bg-white flex items-center justify-left px-4 gap-4">
+            <h1>YourCompiler</h1>
+            <h2>Version</h2>
+        </nav>
+    );
+}
