002c749ae7bbca6147a675d128ecf6cc34ee7f22
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-26 12:42:21 +1100

    dynamicly change output log

--- components/OutputPanel/OutputPanel.tsx
+++ components/OutputPanel/OutputPanel.tsx
@@ -1,7 +1,10 @@
+import UseExecutionContext from "@/contexts/Execution/UseExecutionContext";
+
 export default function OutputPanel() {
+    const {output, setOutput} = UseExecutionContext();
     return (
-        <div className="h-full w-full bg-gray-800">
-            Output Panel
+        <div className="h-full w-full bg-gray-800 text-white p-4">
+            {output}
         </div>
     );
 }
