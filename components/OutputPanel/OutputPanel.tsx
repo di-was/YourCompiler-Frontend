1e191ea5516531f73ed5902fb76484d3526f96df
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-28 22:45:53 +1100

    new color for text

--- components/OutputPanel/OutputPanel.tsx
+++ components/OutputPanel/OutputPanel.tsx
@@ -3,7 +3,7 @@ import UseExecutionContext from "@/contexts/Execution/UseExecutionContext";
 export default function OutputPanel() {
     const {output, setOutput} = UseExecutionContext();
     return (
-        <div className="h-full w-full bg-gray-800 text-white p-4">
+        <div className="h-full w-full bg-primary text-white p-4">
             {output}
         </div>
     );
