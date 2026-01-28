07854ad64f0eb34e682f414f31ab738931b13351
Diwas Dahal <diwasdahal2006@gmail.com>
2026-01-28 22:45:18 +1100

    temporary colours

--- components/Navbar/RunButton.tsx
+++ components/Navbar/RunButton.tsx
@@ -28,10 +28,10 @@ export default function RunButton() {
     }
     return (
         <button onClick={handleClick}
-        className="ml-10 px-4 py-2 bg-green-500 text-white rounded-md
-                   hover:bg-green-600 hover:cursor-pointer hover:ring-2 hover:ring-green-300
-                   active:scale-95 active:brightness-80 active:ring-4 active:ring-green-200 transition">
+        className="ml-10 px-3 py-1 text-white rounded-md border-3 border-secondary
+                   hover:bg-secondary hover:cursor-pointer
+                   active:scale-95 active:brightness-80 transition">
             Run
         </button>
-    );
+    );  
 }
