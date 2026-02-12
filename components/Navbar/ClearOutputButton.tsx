279f378f0f8f66d8412dc4add2b45a0136924a4e
Diwas Dahal <diwasdahal2006@gmail.com>
2026-02-12 16:08:46 +1100

    code for clear outputu utility function

---
+++ components/Navbar/ClearOutputButton.tsx
@@ -0,0 +1,7 @@
+export default function ClearOutputButton() {
+    return (
+        <button className="text-white px-3 py-1 rounded-md border-1 border-white hover:bg-white/10">
+                Clear
+        </button>
+    );
+}
