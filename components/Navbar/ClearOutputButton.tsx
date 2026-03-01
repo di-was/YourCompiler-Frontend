4c441b59d1db0c25e5d6d890fc90bbe67a622c29
Diwas Dahal <diwasdahal2006@gmail.com>
2026-03-01 21:56:28 +1100

    context provider for resetting the output added

--- components/Navbar/ClearOutputButton.tsx
+++ components/Navbar/ClearOutputButton.tsx
@@ -1,6 +1,10 @@
+import UseExecutionContext from "@/contexts/Execution/UseExecutionContext";
+
 export default function ClearOutputButton() {
+    const {setOutput} = UseExecutionContext();
     return (
-        <button className="text-white px-3 py-1 rounded-md border-1 border-white hover:bg-white/10">
+        <button className="text-white px-3 py-1 rounded-md border-1 border-white hover:bg-white/10"
+                onClick={() => setOutput("Output Console - yourcompiler.com")}>
                 Clear
         </button>
     );
